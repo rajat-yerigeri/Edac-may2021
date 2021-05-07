6ed446a2c2a5293b0ef543f06687ad6ea6dce23a
rajat-yerigeri <83281386+rajat-yerigeri@users.noreply.github.com>
2021-05-07 19:37:17 +0530

    Add files via upload

---
+++ assignment1/q2.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+class q2
+
+{
+  public static void main(String args[])
+  
+  { 
+    Scanner sc = new Scanner(System.in);
+	System.out.println("Enter 2 no.s;");
+	int i=sc.nextInt();
+	int j=sc.nextInt();
+	int k=i+j;
+	
+	System.out.println("Addition; "+k);
+	
+	}
+	}
+	
+	
+
