bbbbd822daff7bceabe39c86e48b85feea16c412
rajat-yerigeri <83281386+rajat-yerigeri@users.noreply.github.com>
2021-05-09 00:24:13 +0530

    Add files via upload

---
+++ assignment1/q8.java
@@ -0,0 +1,15 @@
+
+/*
+8. Write a Java program to display the following pattern
+*/
+
+class q8
+{
+public static void main(String args[])
+{
+System.out.println("    J     a    V     V   a");
+System.out.println("    J    a a    V   V   a a");
+System.out.println(" J  J   aaaaa    V V   aaaaa");  
+System.out.println("  JJ   a     a    V   a     a");
+}
+}
