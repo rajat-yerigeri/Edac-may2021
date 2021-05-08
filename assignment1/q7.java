a265853187029d0480fa26248c414803b9f26d9c
rajat-yerigeri <83281386+rajat-yerigeri@users.noreply.github.com>
2021-05-09 00:21:42 +0530

    Add files via upload

---
+++ assignment1/q7.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+class q7
+
+{
+  public static void main (String args[])
+  
+{ 
+ Scanner sc = new Scanner(System.in);
+  
+  System.out.println("enter no; ");
+  
+  int i=sc.nextInt();
+  {
+    for (int j=1;j<=10;j++)
+  
+  System.out.println(i +  "*" + j + "=" + i*j);
+    }
+	}
+	}
+  
+  
+  
+
